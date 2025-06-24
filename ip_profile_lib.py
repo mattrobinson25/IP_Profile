4cda17b9b4787ab5ca03c34e56d908edb12f770b
root <mattrobinson25@protonmail.com>
2025-06-24 18:30:37 -0400

    add type annotations for ip_profile_lib.py

--- ip_profile_lib.py
+++ ip_profile_lib.py
@@ -33,18 +33,18 @@ if not isdir(working_dir):
 
 
 # Log Dates - Some distros may log dates and times slightly differently
-sql_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
-http_log_date = (datetime.now() - timedelta(days=1)).strftime('%d/%b/%Y')
-ssh_log_date = (datetime.now() - timedelta(days=1)).strftime('%b %e')
+sql_date: str = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+http_log_date: str = (datetime.now() - timedelta(days=1)).strftime('%d/%b/%Y')
+ssh_log_date: str = (datetime.now() - timedelta(days=1)).strftime('%b %e')
 # sql connector
-db_con = sqlite3.connect(db_file)
-db_cursor = db_con.cursor()
+db_con: sqlite3.Connection = sqlite3.connect(db_file)
+db_cursor: sqlite3.Cursor = db_con.cursor()
 
 
 my_token : str | None = None
 if api_token_file and isfile(api_token_file):
     with open(api_token_file, 'r') as file:
-        my_tokens : list[str] = file.readlines()
+        my_tokens: list[str] = file.readlines()
 
     for token in my_tokens:
         # scroll through file for token. Ignore commented lines and pick the last token specified.
@@ -52,18 +52,18 @@ if api_token_file and isfile(api_token_file):
             my_token = token
 
     if '?token=' not in my_token:
-        my_token : str = '?token=' + my_token
+        my_token: str = '?token=' + my_token
 
     for whitespace in ['\n', ' ', '\t', '\r']:
         my_token = my_token.replace(whitespace, '')  # replace with nothing. Same as deleting
 else:
-    my_token : None = None
+    my_token: None = None
 
 
 class ApiError(Exception):
     def __init__(self, ip=None):
-        self.ip = ip
-        self.message = f'API returned 404 error. Perhaps an invalid ip address was sent. IP: {self.ip}'
+        self.ips: str = ip
+        self.messages: str = f'API returned 404 error. Perhaps an invalid ip address was sent. IP: {self.ip}'
 
     def __str__(self):
         return self.message
@@ -71,7 +71,7 @@ class ApiError(Exception):
 
 ApiConnectionErrors = (TimeoutError, ReadTimeoutError, ReadTimeout, ApiError, DecodeError, ContentDecodingError)
 
-logger = MyLogger(
+logger: logging.Logger = MyLogger(
     name='ip_profile',
     to_file=logger_file,
     to_console=True,
@@ -80,10 +80,11 @@ logger = MyLogger(
 
 
 def convert_to_sql_type(dictionary: dict) -> dict:
-    sql_types = [int, float, str]
+    sql_types: list[type] = [int, float, str]
 
     for key in dictionary:
-        val, val_type = dictionary[key], type(dictionary[key])
+        val: str = dictionary[key]
+        val_type: type = type(dictionary[key])
 
         if val_type not in sql_types:
             dictionary[key] = json.dumps(val)  # converts to str
@@ -129,10 +130,10 @@ def ip_info(ip_address, token=None) -> dict[str, str | int]:
 
 class IpInfoApi:
     def __init__(self, token=my_token):
-        self.token = token
-        self.failed_requests = []
-        self.successful_requests = []
-        self.count_requests = 0
+        self.token: str = token
+        self.failed_requests: list[dict] = []
+        self.successful_requests: list[dict] = []
+        self.count_requests: int = 0
 
     def ip_request(self, ip_addr) -> dict[str, str | int]:
         # API provided by ipinfo.io
@@ -144,7 +145,7 @@ class IpInfoApi:
             api: str = 'http://ipinfo.io/' + ip_addr  # HTTP (data limits apply)
 
         self.count_requests += 1
-        data =  requests.get(api).json()
+        data: dict = requests.get(api).json()
         self.successful_requests.append(data)
         return data
 
@@ -165,7 +166,7 @@ def log_reader(filename):
 
 def handle_failed_requests(failed_requests: list) -> None:
     if len(failed_requests) > 0:
-        pickle_file = f'{working_dir}/api_error.pickle'
+        pickle_file: str = f'{working_dir}/api_error.pickle'
 
         if not isfile(pickle_file):
             with open(pickle_file, 'wb') as pf:
@@ -174,7 +175,7 @@ def handle_failed_requests(failed_requests: list) -> None:
         logger.warning(f'Api Error occurred. Check {pickle_file}')
 
         with open(pickle_file, 'rb') as pf:
-            failure_data = pickle.load(pf)
+            failure_data: list[dict] = pickle.load(pf)
 
         failed_requests.extend(failure_data)
 
@@ -189,31 +190,31 @@ if trusted_ips_file and isfile(trusted_ips_file):
         for line in f:
             if line and not line.startswith('#'):
                 if '\n' in line:
-                    ip = line.replace('\n', '')
+                    ip: str = line.replace('\n', '')
                 else:
-                    ip = line
+                    ip: str = line
 
                 if line:
                     trusted_ips.append(ip)
 
 
-distro = os_release()['ID'].lower()
+distro: str = os_release()['ID'].lower()
 match distro:
     # find http logs based on distro
     case 'ubuntu' | 'debian':
-        auth_file_dir = '/var/log/apache2/'  # http log files directory
-        ssh_log_files = [f'/var/log/{file}' for file in listdir('/var/log/') if file.startswith('auth.log')]
+        auth_file_dir: str = '/var/log/apache2/'  # http log files directory
+        ssh_log_files: list[str] = [f'/var/log/{file}' for file in listdir('/var/log/') if file.startswith('auth.log')]
 
     case 'centos' | 'rhel' | 'rocky' | 'almalinux' | 'fedora':
-        auth_file_dir = '/var/log/httpd/'  # http log files directory
-        ssh_log_files = [f'/var/log/{file}' for file in listdir('/var/log/') if file.startswith('secure')]
+        auth_file_dir: str = '/var/log/httpd/'  # http log files directory
+        ssh_log_files: list[str] = [f'/var/log/{file}' for file in listdir('/var/log/') if file.startswith('secure')]
 
     case _:
         logging.warning(f'Unsupported OS -- {distro}')
         raise NotImplementedError(f'{distro.title()} is not currently supported.'
                                   f' Only supports Ubuntu, Debian, CentOs, RHEL, Rocky, AlmaLinux, and Fedora')
 
-http_log_files = [
+http_log_files: list[str] = [
     f'{auth_file_dir}/{file}'
     for file in listdir(auth_file_dir)
     if file.startswith('access.log')
